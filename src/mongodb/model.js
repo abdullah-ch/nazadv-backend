067cb70c178a3def8d2c9b36ab73094c366e1cf3
“abdullah-ch” <abdullahchaudharry@gmail.com>
2023-07-12 01:40:04 +0500

    feat: added product schema

--- src/mongodb/model.js
+++ src/mongodb/model.js
@@ -2,11 +2,14 @@
 const mongoose = require('mongoose');
 const userSchema = require('./schema/user');
 const categorySchema = require('./schema/category');
+const productSchema = require('./schema/product');
 
 const userModel = mongoose.model('users', userSchema);
 const categoryModel = mongoose.model('categories', categorySchema);
+const productModel = mongoose.model('products', productSchema);
 
 module.exports = {
   userModel,
   categoryModel,
+  productModel,
 };
---
+++ src/mongodb/schema/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let product = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide the name of the product'],
+      max: [50, 'name should be less than 50 characters'],
+      trim: true,
+      unique: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: [true, 'Please provide the category for the product'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide the price of the product'],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide the description of the product'],
+      max: [1000, 'description should be less than 1000 characters'],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+module.exports = product;
